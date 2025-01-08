bbd5dfa967b8f15f35863add2c014670be87f442
RazorPlay01 <82621554+RazorPlay01@users.noreply.github.com>
2025-01-08 00:44:16 -0500

    🌱 Changelog: - Initial commit
    🐞 Known Bugs:
    -
    
    📋 Backlog:
    -

---
+++ common/src/main/java/com/github/razorplay01/ismah/ISMAH.java
@@ -0,0 +1,13 @@
+package com.github.razorplay01.ismah;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ISMAH {
+    public static final String MOD_ID = "ismah";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static void init() {
+        LOGGER.info("I See My Armored Hand initialized.");
+    }
+}
---
+++ neoforge/src/main/java/com/github/razorplay01/ismah/neoforge/ISMAHNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.razorplay01.ismah.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.github.razorplay01.ismah.ISMAH;
+
+@Mod(ISMAH.MOD_ID)
+public final class ISMAHNeoForge {
+    public ISMAHNeoForge() {
+        ISMAH.init();
+    }
+}
